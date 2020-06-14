0e2ab67108436de80989efacf12cfe05113ed729
David <davidnahum92@gmail.com>
2020-06-14 20:58:19 +0300

    errors handling signup

---
+++ client/src/components/Auth/Signup.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import firebase from "../../firebase";
+
+class Signup extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    errors: [],
+    loading: false,
+  };
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all fields please." };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: "Password is invalid" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirm }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirm.length
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirm }) => {
+    if (password.length < 6 || passwordConfirm.length < 6) {
+      return false;
+    } else if (password !== passwordConfirm) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  showErrors = (errors) =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.isFormValid()) {
+      this.setState({ errors: [], loading: true });
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((createdUser) => {
+          console.log(createdUser);
+          this.setState({ loading: false });
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false,
+          });
+        });
+    }
+  };
+
+  render() {
+    const {
+      username,
+      email,
+      password,
+      passwordConfirm,
+      errors,
+      loading,
+    } = this.state;
+
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="handshake outline" color="red" />
+            Signup for Groupy
+          </Header>
+          <Form size="small" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+                value={username}
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="email"
+                value={email}
+                className={errors.some(error => error.message.toLowerCase().includes('email')) ? 'error' : ''}
+              />
+
+              <Form.Input
+                fluid
+                name="password"
+                icon="user secret"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+                value={password}
+              />
+              <Form.Input
+                fluid
+                name="passwordConfirm"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+                value={passwordConfirm}
+              />
+              <Button
+                disabled={loading}
+                className={loading ? "loading" : ""}
+                color="orange"
+                fluid
+                size="medium"
+              >
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message color="red">
+              <h3>Ops..</h3>
+              {this.showErrors(errors)}
+            </Message>
+          )}
+          <Message>
+            Are you already a groupy?<Link to="/login"> Login here</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default Signup;
