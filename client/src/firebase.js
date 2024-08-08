1501f043820f75af97ae59790b43c1781ef03702
David Nahum <63794877+Davidna92@users.noreply.github.com>
2024-08-08 10:30:42 +0300

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,14 +5,7 @@ import "firebase/storage";
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBO7buOsY9eL4-nu1PunBq5GjC7giwKfHc",
-    authDomain: "groupy-519a9.firebaseapp.com",
-    databaseURL: "https://groupy-519a9.firebaseio.com",
-    projectId: "groupy-519a9",
-    storageBucket: "groupy-519a9.appspot.com",
-    messagingSenderId: "15048817964",
-    appId: "1:15048817964:web:33fa511e0d4d980bcd0089",
-    measurementId: "G-Q9SQPNSCFX"
+  xxxx
   };
 
   firebase.initializeApp(firebaseConfig);
